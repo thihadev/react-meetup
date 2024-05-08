ee403209328ee69d6bc4b8ea8c1d0c6d90a0a329
Thihasoe <thihasoe@comquas.com>
2024-05-08 12:29:14 +0630

    form submit with firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,30 @@
+import { useHistory } from 'react-router-dom'
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-a3361-default-rtdb.asia-southeast1.firebasedatabase.app/meetup.json',
+            {
+                method : 'POST',
+                body : JSON.stringify(meetupData),
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
